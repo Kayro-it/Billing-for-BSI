2ead30ef2a8dfe5624f6abf1c37c833b8195be02
MAhmo0D9 <mahmoodpc2299@gmail.com>
2022-01-23 00:51:49 +0330

    third commit

--- src/CsvReader.java
+++ src/CsvReader.java
@@ -6,16 +6,27 @@ public class CsvReader {
     private String filePath;
     private Scanner scanner;
     private String delimiter;
-    int input;
+    ArrayList<CsvObjectCreator> bill = new ArrayList<CsvObjectCreator>();
+
     public CsvReader(String filePath) throws FileNotFoundException {
     this.filePath = filePath;
     scanner.useDelimiter(delimiter);
     scanner = new Scanner(new File(filePath));
 
     do{
-
-
-    }while((input = scanner.next()) != -1);
+        CsvObjectCreator csv = new CsvObjectCreator();
+        csv.setRow(Integer.parseInt(scanner.next()));
+        csv.setTransactionDate(Integer.parseInt(scanner.next()));
+        csv.setTransactionHour(Integer.parseInt(scanner.next()));
+        csv.setTransactionType(Integer.parseInt(scanner.next()));
+        csv.setTerminalType(Integer.parseInt(scanner.next()));
+        csv.setPrice(Integer.parseInt(scanner.next()));
+        csv.setPresistentNumber(Integer.parseInt(scanner.next()));
+        csv.setLandDate(Integer.parseInt(scanner.next()));
+        csv.setTerminalNumber(Integer.parseInt(scanner.next()));
+        csv.setReferNumber(Integer.parseInt(scanner.next()));
+        bill.add(csv);
+    }while(scanner.hasNext());
     }
 
 
